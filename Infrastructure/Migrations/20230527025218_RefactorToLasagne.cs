d88aa49e2976141b6c0a20c9322ecff444998e98
Matt Goldman <matt.goldman@outlook.com>
2023-05-27 12:52:35 +1000

    Added new migration

---
+++ Infrastructure/Migrations/20230527025218_RefactorToLasagne.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaghettiCommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorToLasagne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "CatalogProducts");
+
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "Products",
+                newName: "StockCount");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "CategoryId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ManufacturerId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products",
+                column: "ManufacturerId",
+                principalTable: "Manufacturers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "StockCount",
+                table: "Products",
+                newName: "Quantity");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Products",
+                newName: "ProductId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ManufacturerId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "CatalogProducts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ManufacturerId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StockCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CatalogProducts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CatalogProducts_Manufacturers_ManufacturerId",
+                        column: x => x.ManufacturerId,
+                        principalTable: "Manufacturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogProducts_CategoryId",
+                table: "CatalogProducts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogProducts_ManufacturerId",
+                table: "CatalogProducts",
+                column: "ManufacturerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products",
+                column: "ManufacturerId",
+                principalTable: "Manufacturers",
+                principalColumn: "Id");
+        }
+    }
+}
